11d87fdc6f36cf9b7318143707a9d5540fe43e7e
Rafael Magana <raf.magana@gmail.com>
2010-07-22 22:17:09 -0500

    cleaned up

--- geek_painter_client.rb
+++ geek_painter_client.rb
@@ -2,7 +2,7 @@ require "rubygems"
 require "geek_painter"
 
 
-painting1 = GeekPainter.paint do
+painting = GeekPainter.paint do
   label "[".in    :brown
   label "\\u".in  :red
   label ":".in    :yellow
@@ -12,14 +12,4 @@ painting1 = GeekPainter.paint do
   label "\$(__git_ps1)".in :blue
 end
 
-painting2 = GeekPainter.paint do
-  label "$ ".in    :yellow
-end
-
-
-#puts painting1.raw_prompt
-
-#puts painting2.for_text
-#puts painting2.for_prompt
-
-puts painting1.prompt
+puts painting.prompt
